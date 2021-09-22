050faf8dc385a95e91a4898edf05a502009820d4
samoeun chhay <samoeunchh@gmail.com>
2021-09-22 10:51:03 +0700

    Add project files.

---
+++ SR6/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SR6.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SR6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ SR6/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR6.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid CategoryId { get; set; }
+        [Required(ErrorMessage ="Category Name field is required!")]
+        [Display(Name ="Category Name")]
+        [MaxLength(50)]
+        public string CategoryName { get; set; }
+        [MaxLength(100)]
+        public string Descrition { get; set; }
+    }
+}
